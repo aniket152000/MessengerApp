914ae23b269fd700aca52745ba948e0b171acf3e
aniket152000 <68470512+aniket152000@users.noreply.github.com>
2022-02-09 17:42:09 +0530

    Add files via upload

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/components/rounded_button.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'chat_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String id='login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _auth= FirebaseAuth.instance;
+  String email;
+  String password;
+  bool showSpinner=false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email=value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(hintText:'Enter Your Email' ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password=value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Password'),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(
+                    title: 'Log In',
+                    colour: Colors.black,
+                    OnPressed: ()async{
+                      setState(() {
+                        showSpinner=true;
+                      });
+                      try{
+                        final newUser=await _auth.signInWithEmailAndPassword(email: email, password: password);
+                        if(newUser!=null){
+                          Navigator.pushNamed(context, ChatScreen.id);
+                        }
+                        setState(() {
+                          showSpinner=false;
+                        }
+                        );
+                      }
+                      catch(e){
+                        print(e);
+                      }
+                    },
+                    ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
